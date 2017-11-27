68d766ec04a30c57c2348f794a0470c0ebd39fbb
Luigi Fugaro <l.fugaro@gmail.com>
2017-11-27 17:12:43 +0100

    ns

--- ocp/install.sh
+++ ocp/install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -x
+
+source /usr/local/s2i/install-teiid-common.sh
+
 injected_dir=$1
-source /usr/local/s2i/install-common.sh
+
+#source /usr/local/s2i/install-common.sh
 #install_modules ${injected_dir}/modules
 #configure_drivers ${injected_dir}/drivers.env
 #source /usr/local/s2i/install-teiid-common.sh
-source /usr/local/s2i/install-teiid-common.sh
-install_deployments ${injected_dir}/DOP.vdb
+echo -n "injected_dir => " ${injected_dir}
+#install_deployments ${injected_dir}/DOP.vdb
+install_deployments /extensions/DOP.vdb
 #configure_translators ${injected_dir}/translators.env
