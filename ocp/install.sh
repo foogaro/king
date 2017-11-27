5355cc778e0d21680768bb381386583f2ea4cee4
Luigi Fugaro <l.fugaro@gmail.com>
2017-11-27 12:49:26 +0100

    Fixev env vars and install script

--- ocp/install.sh
+++ ocp/install.sh
@@ -2,8 +2,9 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-install_deployments ${injected_dir}/DOP.vdb
 #install_modules ${injected_dir}/modules
 #configure_drivers ${injected_dir}/drivers.env
+#source /usr/local/s2i/install-teiid-common.sh
 source /usr/local/s2i/install-teiid-common.sh
+install_deployments ${injected_dir}/DOP.vdb
 #configure_translators ${injected_dir}/translators.env
