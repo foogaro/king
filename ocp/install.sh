f94245ffd4eda2316c1779a04069609b9f66cd27
Luigi Fugaro <l.fugaro@gmail.com>
2017-11-28 17:18:18 +0100

    Luigi user for OData

--- ocp/install.sh
+++ ocp/install.sh
@@ -17,5 +17,7 @@ install_deployments DOP.vdb
 echo -n "DOP.vdb copied"
 #configure_translators ${injected_dir}/translators.env
 echo "Adding the OData role to the King user..."
-/opt/eap/bin/add-user.sh -a --silent=true -u 'king' -p 'king.2017' -g 'admin,connect,odata'
+/opt/eap/bin/add-user.sh -a --silent=true -u "king" -p "king.2017" -g "admin,connect,odata"
 echo "Added the OData role to the King user!"
+/opt/eap/bin/add-user.sh -a --silent=true -u "luigi" -p "luigi.2017" -g "admin,connect,odata"
+echo "Added the OData role to the Luigi user!"
