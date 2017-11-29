241766332470fd5bbaf6db17bdea8a14448d8562
Luigi Fugaro <l.fugaro@gmail.com>
2017-11-29 15:28:50 +0100

    just exit 0

--- ocp/install.sh
+++ ocp/install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -x
+#set -x
 
-source /usr/local/s2i/install-common.sh
-source /usr/local/s2i/install-teiid-common.sh
+#source /usr/local/s2i/install-common.sh
+#source /usr/local/s2i/install-teiid-common.sh
 
-injected_dir=$1
+#injected_dir=$1
 
 #source /usr/local/s2i/install-common.sh
 #install_modules ${injected_dir}/modules
 #configure_drivers ${injected_dir}/drivers.env
 #source /usr/local/s2i/install-teiid-common.sh
 #echo -n "injected_dir => " ${injected_dir}
-install_deployments ${injected_dir}/DOP.vdb
+#install_deployments ${injected_dir}/DOP.vdb
 #install_deployments DOP.vdb
 #configure_translators ${injected_dir}/translators.env
 #echo "Adding the OData role to the King user..."
