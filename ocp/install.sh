0f06c51f7b7704f14afb643e00fde094fd25702b
Luigi Fugaro <l.fugaro@gmail.com>
2017-11-29 15:46:09 +0100

    modules drivers user luigi

--- ocp/install.sh
+++ ocp/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#set -x
+set -x
+
+source /usr/local/s2i/install-common.sh
+source /usr/local/s2i/install-teiid-common.sh
+
+injected_dir=$1
 
 #source /usr/local/s2i/install-common.sh
-#source /usr/local/s2i/install-teiid-common.sh
-
-#injected_dir=$1
-
-#source /usr/local/s2i/install-common.sh
-#install_modules ${injected_dir}/modules
-#configure_drivers ${injected_dir}/drivers.env
+install_modules ${injected_dir}/modules
+configure_drivers ${injected_dir}/drivers.env
 #source /usr/local/s2i/install-teiid-common.sh
 #echo -n "injected_dir => " ${injected_dir}
 #install_deployments ${injected_dir}/DOP.vdb
@@ -17,8 +17,8 @@
 #configure_translators ${injected_dir}/translators.env
 #echo "Adding the OData role to the King user..."
 #/opt/eap/bin/add-user.sh -a --silent=true -u "king" -p "king.2017" -g "admin,connect,odata"
-#echo "Adding the OData role to the user LUIGI..."
-#/opt/eap/bin/add-user.sh -a --silent=true -u "luigi" -p "luigi.2017" -g "admin,connect,odata,user"
-#echo "Added the OData role to the user LUIGI"
+echo "Adding the OData role to the user LUIGI..."
+/opt/eap/bin/add-user.sh -a --silent=true -u "luigi" -p "luigi.2017" -g "admin,connect,odata,user"
+echo "Added the OData role to the user LUIGI"
 
 exit 0
