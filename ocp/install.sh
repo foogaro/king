bfd3fc9ae4f1dbbf9668ce5187fb1618cdf43979
Luigi Fugaro <l.fugaro@gmail.com>
2017-11-29 10:00:10 +0100

    Removed the add-user.sh invocation

--- ocp/install.sh
+++ ocp/install.sh
@@ -16,8 +16,3 @@ echo -n "injected_dir => " ${injected_dir}
 install_deployments DOP.vdb
 echo -n "DOP.vdb copied"
 #configure_translators ${injected_dir}/translators.env
-echo "Adding the OData role to the King user..."
-/opt/eap/bin/add-user.sh -a --silent=true -u "king" -p "king.2017" -g "admin,connect,odata"
-echo "Added the OData role to the King user!"
-/opt/eap/bin/add-user.sh -a --silent=true -u "luigi" -p "luigi.2017" -g "admin,connect,odata"
-echo "Added the OData role to the Luigi user!"
